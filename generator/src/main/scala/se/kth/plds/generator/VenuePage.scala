b7b5d13b5accff8668cbd135f141243848eb2c2e
Lars Kroll <lkroll@kth.se>
2020-02-06 17:37:28 +0100

    Better map. Fixes #1

--- generator/src/main/scala/se/kth/plds/generator/VenuePage.scala
+++ generator/src/main/scala/se/kth/plds/generator/VenuePage.scala
@@ -32,10 +32,21 @@ class VenuePage(val talks: List[Talk] = Nil) {
       ),
       div(
         BootstrapStyle.container,
-        img(BootstrapStyle.shadowSmall,
-            BootstrapStyle.imgFluid,
-            src := "https://discan18.github.io/assets/images/direction.png",
-            alt := "Map of Electrum in Kista")
+        div(
+          cls := "map-responsive shadow",
+          iframe(
+            src := "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2030.694763406302!2d17.94730971607906!3d59.404804881685365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9eed3364c17f%3A0x7e32b513440b6999!2sElectrum!5e0!3m2!1sen!2sse!4v1581004222915!5m2!1sen!2sse",
+            width := "600",
+            height := "450",
+            attr("frameborder") := "0",
+            style := "border:0;",
+            attr("allowfullscreen") := ""
+          )
+        )
+        // img(BootstrapStyle.shadowSmall,
+        //     BootstrapStyle.imgFluid,
+        //     src := "https://discan18.github.io/assets/images/direction.png",
+        //     alt := "Map of Electrum in Kista")
       )
     );
 }
